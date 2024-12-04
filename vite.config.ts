805ea7d20d27435947129f51f07648883cf6043e
Jason Li <Jason.Li8885551@gmail.com>
2024-12-03 16:36:40 -0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'TypeScriptPartFinalCMPM121-TEST', // Replace <REPO_NAME> with your repository name
+  base: '/TypeScriptPartFinalCMPM121-TEST/', // Replace <REPO_NAME> with your repository name
+
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 });
